697f2889a9592a3b3c63c5e958d4b79b3e37a1ce
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 17:09:43 +0900

    配送料の負担追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :Status
+  belongs_to :cost
 
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
