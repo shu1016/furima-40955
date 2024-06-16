513d1b855d75bc122672bd203a3ee43de11eab23
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 21:27:52 +0900

    画像投稿とpriceのバリテーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,10 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :content, presence: true
-  validates :price, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
