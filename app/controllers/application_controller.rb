5d342a6843307a7c4e2c5b46fdffd11453e2d84d
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 15:35:23 +0900

    再指摘箇所修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :configure_permited_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permited_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday ])
   end
 end
