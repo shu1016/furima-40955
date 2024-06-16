85aa9bb5a7d611fb9d135a14b0e6f25683ef87f5
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 17:17:05 +0900

    発送元地域追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :Status
   belongs_to :cost
+  belongs_to :region
 
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
