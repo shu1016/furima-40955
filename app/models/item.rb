4d0b996a3a9f46bde33cb169a7ba8ab6a2007427
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 18:08:58 +0900

    createアクション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :price, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :Status
