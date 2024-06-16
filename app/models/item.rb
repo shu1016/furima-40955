01c0565ea3f1ad764f8e65810e7cdb3245325187
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 17:06:11 +0900

    商品の状態追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,9 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :Status
+
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
