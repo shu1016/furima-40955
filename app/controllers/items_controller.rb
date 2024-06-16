a563b11932e9b7f9ad6826c67435c7117bcc5ce9
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 16:28:13 +0900

    item_params 追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :status_id, :cost_id, :region_id, :period_id, :image).merge(user_id: current_user.id)
+  end
+
 end
