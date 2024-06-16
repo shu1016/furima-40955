d4651bfafba84d71be8512b33b152a51db42d70d
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-16 17:20:29 +0900

    発送までの日数追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,12 @@ class Item < ApplicationRecord
   belongs_to :Status
   belongs_to :cost
   belongs_to :region
+  belongs_to :period
 
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :period_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
