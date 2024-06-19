5159417cb39c6e5b58a70624e28cc8e3017d9c11
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-19 16:54:09 +0900

    createアクション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,20 @@ class OrdersController < ApplicationController
     @order_delivery = OrderDelivery.new
   end
 
+  def create
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:post_code, :region, :city, :address, :building, :tel_number).merge(user_id: current_user.id, item_id: item_id)
+  end
+
 end
