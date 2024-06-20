3add5118d89bcf8ec8e0b7fdb5bb5359cca652f6
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-20 09:22:22 +0900

    テストコード実装

---
+++ spec/factories/order_deliveries.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_delivery do
+    post_code             {"111-1234"}
+    region_id             {Region.where.not(id: 1).sample.id}
+    city                  {"横浜市"}
+    address               {"青山町1-1-1"}
+    building              {"第一ビル"}
+    tel_number            {"09011112222"}
+  end
+end
