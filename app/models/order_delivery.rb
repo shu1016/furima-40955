cb8b3780d9a6dfdf59f515974c9368316ebe3ff5
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-20 09:04:32 +0900

    バリテーション修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -2,11 +2,12 @@ class OrderDelivery
   include ActiveModel::Model
   attr_accessor :post_code, :region_id, :city, :address, :building, :tel_number, :user_id, :item_id
 
+  
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :address
-    validates :tel_number, format: {with: /\A[0-9]{9,11}\z/}
+    validates :tel_number, format: {with: /\A[0-9]{10,11}\z/}
     validates :user_id
     validates :item_id
   end
